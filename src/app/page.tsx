6097693096a394231d203da4e042faa9de255fea
Westrose Team <westrose@interiors.ca>
2026-03-09 20:35:06 +0000

    Fix: Add tsconfig and use relative imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import Portfolio from '@/components/Portfolio'
-import Services from '@/components/Services'
-import WhyChooseUs from '@/components/WhyChooseUs'
-import ContactForm from '@/components/ContactForm'
-import Footer from '@/components/Footer'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Portfolio from '../components/Portfolio'
+import Services from '../components/Services'
+import WhyChooseUs from '../components/WhyChooseUs'
+import ContactForm from '../components/ContactForm'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
